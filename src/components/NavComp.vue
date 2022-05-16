3a6b1450a81eff16be14951f3950c5b45b943bd4
AlessandraMarusi <alessandra.marusi@gmail.com>
2022-05-16 13:37:40 +0200

    Aggiunto dati.json con arrays

--- src/components/NavComp.vue
+++ src/components/NavComp.vue
@@ -1,13 +1,37 @@
 <template>
-    <div><h1>NavBar</h1></div>
+    <nav>
+        <img src="../assets/img/theme_eduprime_logo.png" alt="Edu prime logo"> 
+        <ul>
+            <li v-for="(item, index) in datiNav" :key="index">
+                <h2>{{item.title}}</h2>
+            </li>
+        </ul>
+        <button @click="checkApp">Check</button>
+    </nav>
 </template>
 
 <script>
+import data from '../dati.json'
 export default {
-    name: 'NavComp'
+    name: 'NavComp',
+    data(){
+        return{
+            datiNav: data.navItems
+        }
+    },
+    methods:{
+        checkApp(){
+            console.log(this.datiNav)
+        }
+    },
+    
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+@import "../style/general";
+    nav {
+        background-color: $bgColor;
+    }
+    
 </style>
