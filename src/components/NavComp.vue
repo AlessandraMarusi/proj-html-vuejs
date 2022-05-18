59892dc711c632df6ef87b03cb172455dbe62b7b
AlessandraMarusi <alessandra.marusi@gmail.com>
2022-05-18 17:40:14 +0200

    Navbar animata

--- src/components/NavComp.vue
+++ src/components/NavComp.vue
@@ -9,10 +9,13 @@
             <div class="headerLogo">
             <img src="../assets/img/theme_eduprime_logo.png" alt="Edu prime logo"> 
         </div>
-        <ul class="m-0">
-            <li class="m-0" v-for="(item, index) in datiNav" :key="index">
-                <h5 class="m-0" v-if="item.link !== true">{{item.title}} <span class="arrowMenu">&#9660;</span></h5>
+        <ul class="m-0 navItems">
+            <li class="m-0" :class="{menuItem :! item.link}" v-for="(item, index) in datiNav" :key="index">
+                <a class="menuLink" v-if="item.link !== true">{{item.title}} <span class="arrowMenu">&#9660;</span></a>
                 <a v-else :href="item.url"><h5  class="m-0">{{item.title}}</h5></a>
+                <ul class="dropdownMenu">
+                    <li v-for="(dropdownItem, index) in item.menuItems" :key="index">{{dropdownItem.txt}}</li>
+                </ul>
             </li>
         </ul>
         <button class="secBtn bigBtn m-0" @click="checkApp"><a href="#"><h4 class="m-0">View Courses</h4></a></button>
@@ -57,19 +60,59 @@ export default {
                     width: 300px;
                 }
             }
-            ul {
+            .navItems {
                 display: flex;
                 align-items: center;
                 list-style: none;
                 gap: 30px;
                 padding-right: 30px;
+                li {
+                    cursor: pointer;
+                }
                 .arrowMenu {
                     font-size: 0.5rem;
+                }
+                a:hover{
+                    color: $txtColorHover;
+                }
+                /* .menuItem {
+                    position: relative;
+                } */
+                .menuItem:hover .dropdownMenu {
+                    opacity: 1;
+                    visibility: visible;
+                    top: 70%;
+                }
+                .dropdownMenu{
+                    color: $titleColor;
+                    border-radius: 10px;
+                    text-align: left;
+                    font-weight: 900;
+                    opacity: 0;
+                    position: absolute;
+                    background: #fff;
+                    width: 200px;
+                    top: 120%;
+                    transition: all 250ms ease-in-out;
+                    -webkit-transition: all 250ms ease-in-out;
+                    visibility: hidden;
+                    padding: 15px;
+                    box-shadow: 0 0 20px rgb(67 73 89 / 10%);
+                    -webkit-box-shadow: 0 0 20px rgb(67 73 89 / 10%);
+                    list-style: none;
+                    li {
+                        padding: 5px;
+                        transition: 0.5s;
+                    }
+                    li:hover {
+                        color: $txtColorBg;
+                    }
                 }
             }
             button {
                 h4 {
                     text-transform: uppercase;
+                    font-size: 1.5rem;
                 }
             }
         }
