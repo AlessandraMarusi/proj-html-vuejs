22ade9cbc77a6c04544716492fc0e618bf8cd952
AlessandraMarusi <alessandra.marusi@gmail.com>
2022-05-19 16:09:04 +0200

    Cambi vari di stile

--- src/components/StickyComp.vue
+++ src/components/StickyComp.vue
@@ -8,8 +8,9 @@
     <div class="leftButtons">
         <div class="tooltipContainer">
             <span>Purchase EduPrime on Envato Market</span>
+            <div class="triangle">&#9662;</div>
         </div>
-        <button>
+        <button class="purchaseBtn">
             <img src="../assets/svg/svg-3.svg" alt="">
             <span>Purchase Theme</span>
         </button>
@@ -19,13 +20,41 @@
         </button>
 
     </div>
-    <div class="rightArrow"></div>
+    <a href="#navBar">
+        <div class="rightArrow" :class="{myVisible : isVisible}">
+            <img src="../assets/img/back-to-top-arrow.svg" alt="">
+        </div>
+    </a>
 </div>
 </template>
 
 <script>
 export default {
-    name: "StickyComp"
+    name: "StickyComp",
+    data(){
+        return{
+            isVisible: false,
+            y: window.scrollY
+        }
+    },
+    created () {
+    window.addEventListener('scroll', this.handleScroll);
+    },
+    destroyed () {
+        window.removeEventListener('scroll', this.handleScroll);
+    },
+    methods:{
+        handleScroll(){
+            /* y = window.scrollY */
+            this.y = window.scrollY
+            if (this.y >= 400) {
+                this.isVisible = true
+            }
+            else {
+                this.isVisible = false
+            }
+        }
+    },
 }
 </script>
 
@@ -44,9 +73,15 @@ export default {
     border-radius: 0px 10px 10px 0px;
     box-shadow: 0 0 25px rgb(37 37 37 / 40%);
     z-index: 444;
+    transition: 00.5s;
     svg {
         padding: 12px;
+        cursor: pointer;
+        transition: 00.5s;
     }
+}
+.sideMenu:hover svg{
+    padding: 12px 15px;
 }
 .leftButtons {
     position: fixed;
@@ -55,14 +90,36 @@ export default {
     z-index: 444;
     display: flex;
     flex-flow: column nowrap;
+    align-items: center;
+    /* justify-content: center; */
     gap: 15px;
+    .tooltipContainer {
+        width: 170px;
+        background-color: #1A1A1A;
+        color: $white;
+        padding: 5px;
+        border-radius: 5px;
+        font-size: 00.8rem;
+        box-shadow: 0 17px 10px -12px rgba(0, 0, 0, .4);
+        text-align: center;
+        position: relative;
+        .triangle {
+            position: absolute;
+            bottom: 0%;
+            left: 50%;
+            transform: translate(-50%, 60%);
+            font-size: 2rem;
+            color: #1A1A1A;
+        }
+    }
     button {
-        background-color: $titleColor;
+        background-color: #454545;
         color: $txtColorOnBg;
-        padding: 8px 30px;
+        padding: 10px 25px;
         border: none;
         border-radius: 5px;
         box-shadow: 0 17px 10px -12px rgba(0, 0, 0, .4);
+        transition: all 0.25s ease-in-out;
         img {
             width: 15px;
         }
@@ -71,5 +128,38 @@ export default {
             padding-left: 5px;
         }
     }
+    button:hover {
+        background-color:  #d77864;
+        transform: translate(0%, -5%);
+    }
+    .purchaseBtn:hover {
+        background-color: #81b441;
+        img {
+            /* filter: grayscale(100%); */
+            filter: contrast(0%);
+        }
+    }
+}
+.rightArrow {
+    position: fixed;
+    bottom: 5%;
+    right: 2%;
+    background-color: $btnColorSec;
+    color: $white;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 444;
+    visibility: hidden;
+    transition: 00.5s;
+}
+.rightArrow:hover {
+    background-color: $bgColor;
+}
+.myVisible {
+    visibility: visible;
 }
 </style>
